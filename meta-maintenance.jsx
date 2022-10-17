20227a9f59d0504c4915d627d2cd37b18a019ccf
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-10-17 19:30:02 +0900

    test

--- meta-maintenance.jsx
+++ meta-maintenance.jsx
@@ -2,7 +2,16 @@ export const title = "meta-maintenance";
 
 export default (data) => (
   <>
-    <h1>{data.title}</h1>
-    <p>meta-maintenanceのページ</p>
+    <header>
+      <h1>{data.title}</h1>
+    </header>
+    <main>
+      <p>meta-maintenanceのページ</p>
+    </main>
+    <footer>
+      <ul>
+        <li>GitHub: https://github.com/piderlab/piderlab.github.io</li>
+      </ul>
+    </footer>
   </>
 );
